e6029942851ceec471d8bba769dfb36c74651fd5
EduardoReynaU <eduardojuan.reyna@unmsm.edu.pe>
2022-07-18 14:26:57 -0500

    Lb02

--- SanMarTec/02 Desarrollo/PWES/router/index.js
+++
@@ -1,9 +0,0 @@
-const { Router} = require("express");
-const routerIndex = Router();
-const renderIndex = require('../controllers/index');
-
-
-
-routerIndex.get('/', renderIndex);
-
-module.exports = routerIndex;
--- SanMarTec/02 Desarrollo/PWES/router/nosotros.js
+++
@@ -1,8 +0,0 @@
-const {Router} = require('express');
-const { renderNosotros } = require('../controllers/nosotros');
-const routerNosotros = Router();
-
-routerNosotros.get('/', renderNosotros);
-
-
-module.exports = routerNosotros;
